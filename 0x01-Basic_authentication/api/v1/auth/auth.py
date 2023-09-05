187a141afdae55a8beabeafd172ce0775d54fa47
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-09-06 01:08:30 +0300

    auth update

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
-"""API authentication."""
-from flask import request
+"""API Authentication.
+"""
 import re
 from typing import List, TypeVar
+from flask import request
 
 
 class Auth:
-    """ manages API authentication """
-
-    def __init__(self):
-        pass
-
+    """Authentication class.
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Check if path requires authorization. """
+        """Checks if a path requires authentication.
+        """
         if path is not None and excluded_paths is not None:
             for exclusion_path in map(lambda x: x.strip(), excluded_paths):
                 pattern = ''
@@ -27,11 +26,13 @@ class Auth:
         return True
 
     def authorization_header(self, request=None) -> str:
-        """ Gets authorization header from requests. """
+        """Gets the authorization header field from the request.
+        """
         if request is not None:
             return request.headers.get('Authorization', None)
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ Get current user from request. """
+        """Gets the current user from the request.
+        """
         return None
